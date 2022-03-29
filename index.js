2d07215518c7de906b939c9b6a0801aaa2f4c76d
Chrisahn75 <chris.ahona@gmail.com>
2022-03-29 17:33:40 +0200

    try to fix address update

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ dotenv.config({
     path: "./config.env",
   });
 const app = express();
-
 app.use(express.json());
 
 mongoose
@@ -19,7 +18,35 @@ mongoose
 	.then(() => console.log("Connected to MongoDB"));
 
 
+app.get("/", (_req, res) => {
+    res.send("Populate");
+});
 
+app.get("/students", async (_req, res) => {
+	const students = await student.find();
+
+	res.json(students);
+});
+app.get("/students/:studentId", async (req, res) => {
+    const students = await student.findById(req.params.studentId).populate(
+    "address"
+);
+    res.json(students);
+});
+
+app.post("/students", async (req, res) => {
+    await student.create(req.body);
+    res.status(201).send("Student created");
+});
+    
+app.post("/students/:studentId/address", async (req, res) => {
+    const addresse = await address.create(req.body);
+    await student.findByIdAndUpdate(req.params.studentId, {
+    $push: { address: addresse._id },
+});
+    res.status(201).send("Address created");
+});
+    
 app.use("*", (err, req, res, next) => {
     res.send("error");
 });
